456bb153443fd965df8b09f49089effff8204f4f
prkm93 <aradshub93@gmail.com>
2021-09-13 10:22:00 +0530

    updated js file

---
+++ index.js
@@ -0,0 +1,13 @@
+const btnSubmit = document.querySelector("#btn-submit");
+const birthday = document.querySelector('#birthday');
+
+const checkPalindrome = () => {
+    console.log(birthday.value);
+
+}
+
+const dateFormats = (date) => {
+    
+}
+
+btnSubmit.addEventListener('click', checkPalindrome);
